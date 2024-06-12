bdb26bac5f40b711c95a416cd69fd3d17b2944df
dmitry <dimabogacan@gmail.com>
2024-06-12 15:41:30 +0200

    completed fourth container

---
+++ frontend/src/app/fourthContainer/data.tsx
@@ -0,0 +1,17 @@
+export const fourthData = [
+  {
+    img: "/room1.png",
+    h3: "01 Bed Room",
+    p: "Inner Peace",
+  },
+  {
+    img: "/room1.png",
+    h3: "01 Bed Room",
+    p: "Inner Peace",
+  },
+  {
+    img: "/room1.png",
+    h3: "01 Bed Room",
+    p: "Inner Peace",
+  },
+];
--- frontend/src/app/fourthContainer/fourthContainer.tsx
+++ frontend/src/app/fourthContainer/fourthContainer.tsx
@@ -3,32 +3,53 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/effect-coverflow";
 import "./fourthContainer.scss";
-import { Pagination, Navigation } from "swiper/modules";
+import { Pagination, Navigation, EffectCoverflow } from "swiper/modules";
+import { fourthData } from "./data";
 
 export const FourthContainer = () => {
+  const sliderData = fourthData.map((obj, index) => {
+    return (
+      <SwiperSlide key={index}>
+        <div>
+          <img src={obj.img} alt="" />
+          <div className="slideCaption">
+            <h3>{obj.h3}</h3>
+            <p>{obj.p}</p>
+          </div>
+        </div>
+      </SwiperSlide>
+    );
+  });
   return (
     <div className="fourthContainer">
+      <div className="fourthTextContainer">
+        <h2>50+ Beautiful rooms inspiration</h2>
+        <p>
+          Our designer already made a lot of beautiful prototipe of rooms that
+          inspire you
+        </p>
+        <a href="">Explore More</a>
+      </div>
       <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        pagination={{
-          clickable: true,
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
         }}
         navigation={true}
-        modules={[Pagination, Navigation]}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {sliderData}
       </Swiper>
     </div>
   );

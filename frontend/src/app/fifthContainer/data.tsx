008ece1f35f25e325ffb945734919be5c5282106
dmitry <dimabogacan@gmail.com>
2024-06-13 05:55:21 +0200

    fifth container completed

---
+++ frontend/src/app/fifthContainer/data.tsx
@@ -0,0 +1,5 @@
+export const fifthData = [
+  {
+    icon: "/Share.png",
+  },
+];
--- frontend/src/app/fifthContainer/fifthContainer.tsx
+++ frontend/src/app/fifthContainer/fifthContainer.tsx
@@ -1,10 +1,21 @@
 import styles from "./fifthContainer.module.scss";
+import { fifthData } from "./data";
 
 export const FifthContainer = () => {
+  const divItems = fifthData.map((obj, index) => {
+    return (
+      <div
+        className={styles.divIconContainer}
+        style={{ background: `url(${obj.icon}) no-repeat center/cover` }}
+        key={index}
+      ></div>
+    );
+  });
   return (
     <div className={styles.fifthContainer}>
       <span>Great</span>
       <h2>#FuniroFurniture</h2>
+      {divItems}
     </div>
   );
 };
